8d1a55945b7231c217570fd74612918f3c4a1279
Romulo Monteiro <148643335+RomuloMonteio@users.noreply.github.com>
2025-12-17 00:17:14 +0000

    stached

--- Projeto/Proposta/Proposta final/app/NBA/FormClassificacao.Designer.cs
+++ Projeto/Proposta/Proposta final/app/NBA/FormClassificacao.Designer.cs
@@ -48,10 +48,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(357, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(476, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 26);
+            this.label1.Size = new System.Drawing.Size(176, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Classificacao";
             // 
@@ -61,21 +60,23 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1464, 54);
+            this.panel1.Size = new System.Drawing.Size(1924, 66);
             this.panel1.TabIndex = 86;
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // btmOeste
             // 
-            this.btmOeste.Location = new System.Drawing.Point(509, 144);
+            this.btmOeste.Location = new System.Drawing.Point(679, 177);
+            this.btmOeste.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btmOeste.Name = "btmOeste";
-            this.btmOeste.Size = new System.Drawing.Size(75, 23);
+            this.btmOeste.Size = new System.Drawing.Size(100, 28);
             this.btmOeste.TabIndex = 90;
             this.btmOeste.Text = "Oeste";
             this.btmOeste.UseVisualStyleBackColor = true;
@@ -83,9 +84,10 @@
             // 
             // btmLeste
             // 
-            this.btmLeste.Location = new System.Drawing.Point(428, 144);
+            this.btmLeste.Location = new System.Drawing.Point(571, 177);
+            this.btmLeste.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btmLeste.Name = "btmLeste";
-            this.btmLeste.Size = new System.Drawing.Size(75, 23);
+            this.btmLeste.Size = new System.Drawing.Size(100, 28);
             this.btmLeste.TabIndex = 89;
             this.btmLeste.Text = "Leste";
             this.btmLeste.UseVisualStyleBackColor = true;
@@ -94,30 +96,34 @@
             // Conferencia
             // 
             this.Conferencia.AutoSize = true;
-            this.Conferencia.Location = new System.Drawing.Point(345, 149);
+            this.Conferencia.Location = new System.Drawing.Point(460, 183);
+            this.Conferencia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Conferencia.Name = "Conferencia";
-            this.Conferencia.Size = new System.Drawing.Size(64, 13);
+            this.Conferencia.Size = new System.Drawing.Size(79, 16);
             this.Conferencia.TabIndex = 91;
             this.Conferencia.Text = "Conferencia";
             this.Conferencia.Click += new System.EventHandler(this.label2_Click);
             // 
             // classificacao
             // 
-            this.classificacao.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.classificacao.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.classificacao.BackgroundColor = System.Drawing.Color.White;
             this.classificacao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.classificacao.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.classificacao.Location = new System.Drawing.Point(348, 180);
+            this.classificacao.Location = new System.Drawing.Point(464, 222);
+            this.classificacao.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.classificacao.Name = "classificacao";
-            this.classificacao.Size = new System.Drawing.Size(523, 388);
+            this.classificacao.RowHeadersWidth = 51;
+            this.classificacao.Size = new System.Drawing.Size(697, 478);
             this.classificacao.TabIndex = 88;
             this.classificacao.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.classificacao_CellContentClick);
             // 
             // btmTodos
             // 
-            this.btmTodos.Location = new System.Drawing.Point(590, 144);
+            this.btmTodos.Location = new System.Drawing.Point(787, 177);
+            this.btmTodos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btmTodos.Name = "btmTodos";
-            this.btmTodos.Size = new System.Drawing.Size(75, 23);
+            this.btmTodos.Size = new System.Drawing.Size(100, 28);
             this.btmTodos.TabIndex = 92;
             this.btmTodos.Text = "All";
             this.btmTodos.UseVisualStyleBackColor = true;
@@ -134,19 +140,20 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1464, 646);
+            this.panel2.Size = new System.Drawing.Size(1924, 795);
             this.panel2.TabIndex = 93;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // FormClassificacao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1464, 646);
+            this.ClientSize = new System.Drawing.Size(1924, 795);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormClassificacao";
             this.Text = "FormLigas";
             this.Load += new System.EventHandler(this.FormClassificacao_Load);
